c060df1b21b21d726235b56e89d0ea8012894e43
ZebraSeller <yuligent.ycg@gmail.com>
2022-10-22 01:14:33 -0500

    Revert "a"
    This reverts commit ce949cd6df18c3cf723dd8f3e7bca19e47ba027f.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {HashRouter, Route, Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 //import {useLocation} from 'react-router-dom';
 import NavBar from './parts/navbar';
 import Home from './pages/home';
@@ -13,17 +13,15 @@ import './App.css';
 function App() {
   return (
       <div className="App">
-        <HashRouter basename={process.env.PUBLIC_URL}>
-          <NavBar navTitle="MP2 PokeAPI"/>
-          <Routes>
-            <Route exact path="/" element={<Home />}/>
-            <Route path="/list" element={<List />}/>
-            <Route path="/gallery" element={<Gallery />}/>
-            <Route path="/detail" element={<Detail />}/>
-            <Route path="*" element={<NotFound />}/>
-            <Route element={<NotFound />}/>
-          </Routes>
-        </HashRouter>
+        <NavBar navTitle="MP2 PokeAPI"/>
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route path="/list" element={<List />}/>
+          <Route path="/gallery" element={<Gallery />}/>
+          <Route path="/detail" element={<Detail />}/>
+          <Route path="*" element={<NotFound />}/>
+          <Route element={<NotFound />}/>
+        </Routes>
       </div>
   );
 }
